217601841477b4db1960095ad10a93b9fa3e6a5b
st13g <cristdelc@gmail.com>
2020-04-02 16:23:06 -0500

    fix

--- client2.py
+++ client2.py
@@ -1,21 +1,29 @@
 import socket
 
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect((socket.gethostname(),1235))
+HEADERSIZE = 10
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 1243))
+
 while True:
-	full_msg = ''
-	new_msg = True
-	while True:
-		msg = s.recv(16)
-		if new_msg:
-			print(f"new message length: {msg[HEADERSIZE]}")
-			msglen = int(msg[:HEADERSIZE])
-			new_msg = False
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(16)
+        if new_msg:
+            print("new msg len:",msg[:HEADERSIZE])
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False
 
-		full_msg += msg.decode("utf-8")
+        print(f"full message length: {msglen}")
 
-		if len(full_msg)- HEADERSIZE ==msglen:
-			print("full msg recvd")
-			print(full_msg[HEADERSIZE:])
-			new_msg = True
-	print(full_msg)
+        full_msg += msg.decode("utf-8")
+
+        print(len(full_msg))
+
+
+        if len(full_msg)-HEADERSIZE == msglen:
+            print("full msg recvd")
+            print(full_msg[HEADERSIZE:])
+            new_msg = True
+            full_msg = ""
