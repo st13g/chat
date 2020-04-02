f96c74526fc9f09bb8ff8c41f4fa5e28588472ab
st13g <cristdelc@gmail.com>
2020-04-02 15:24:20 -0500

    adding new files

---
+++ server2.py
@@ -0,0 +1,21 @@
+import socket
+import time
+HEADERSIZE=10
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((socket.gethostname(),1235))
+s.listen(5)
+
+while True:
+	clientsocket,address = s.accept()
+	print(f"Connection from {address} has been stablished")
+
+	msg = "Welcome to the server"
+	msg = f'{len(msg)<:{HEADERSIZE}}' + msg
+
+	clientsocket.send(bytes(msg, "utf-8"))
+	while True:
+		time.sleep(3)
+		msg = f"the time is {time.time()}"
+		msg = f'{len(msg)<:{HEADERSIZE}}' + msg
+		clientsocket.send(bytes(msg, "utf-8"))
